85f38b557c22bc999d68967233229905135357ed
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-01-23 12:05:30 +0300

    add yandex.cloud VM with nat

--- modules/services/yandex-cloud/main.tf
+++ modules/services/yandex-cloud/main.tf
@@ -8,9 +8,66 @@ terraform {
 }
 
 provider "yandex" {
-  zone = "ru-central1-c"
+  zone = "ru-central1-d"
+  folder_id = "b1ggegfk5mo7j1ck1p4o"
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-d"
+  size     = "20"
+  image_id = "fd8m5hqeuhbtbhltuab4"
+}
+
+resource "yandex_compute_disk" "boot-disk-2" {
+  name     = "boot-disk-2"
+  type     = "network-hdd"
+  zone     = "ru-central1-d"
+  size     = "20"
+  image_id = "fd8m5hqeuhbtbhltuab4"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet-1"
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "yandex_compute_instance" "vm-1" {
   name = "terraform-1"
+  zone = "ru-central1-d"
+  platform_id = "standard-v2"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_adress_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
